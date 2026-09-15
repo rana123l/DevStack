e2cec7357b494ba1da8afc465f182898f30c7eab
sakib-uddin-rana <rana11sakib@gmail.com>
2026-09-16 01:36:13 +0600

    Complete Footer & responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Footer />
     </>
   );
 }
